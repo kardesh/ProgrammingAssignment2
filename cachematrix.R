ceb2d93fdbf862fe520309ef3aeb251f47b42e8d
kardesh <kartikdeshmukh.2389@gmail.com>
2015-07-25 15:06:50 +0530

    Edit cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCachematrix() is a function to create a matrix object.
+## It also assigns a list of functions to it.
+## cacheSolve() is a function used to get inverse of matrix.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCachematrix() creates an object of type matrix.
+## Functions such as fetching matrix,inverse of matrix and
+## changing values of both matrix and inverse of matrix are defined.
 
+makeCachematrix <- function(x = matrix())
+{
+        inv <- NULL				# Initializing Inverse of Matrix
+        set <- function(y = matrix())	# Changing Matrix object
+	  {
+                if(!identical(x,y))		# Check to compare old and new matrix
+		   {
+		    message("Change matrix")
+		    x <<- y
+                inv <<- NULL
+		   }
+		    else
+		   {
+   		    message("Matrix unchanged")
+		   }
+        }
+        get <- function() x			# Returning Matrix object
+        setinv <- function(i = matrix()) inv <<- i	# Changing Inverse of matrix
+        getinv <- function() inv		# Returning Inverse of Matrix
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() returns inverse of matrix object.
+## This is done by either getting value in cache
+## or calculating the inverse.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+        inv <- x$getinv()		# Checking if Cache holds a value
+        if(!is.null(inv)) {
+                message("Getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)		# Calculating inverse of a matrix
+        x$setinv(inv)
+        inv
 }
